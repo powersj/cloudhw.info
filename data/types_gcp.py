7df164ea6e9269275027c0a19709d4540d9bb181
Josh Powers <powersj@fastmail.com>
2024-01-14 16:49:57 -0700

    chore: Updates to azure and gcp type gather
    Print errors, correct parsing of messages

--- data/types_gcp.py
+++ data/types_gcp.py
@@ -16,11 +16,11 @@ def gce_instance_types():
         "machine-types",
         "list",
         "--format=json",
-        '--filter="zone:(us-west1)"',
     ]
     output = subprocess.run(cmd, capture_output=True)
     if output.returncode != 0:
         print("Oops: gcloud compute machine-types command failed!")
+        print(output.stderr.decode("utf-8"))
         sys.exit(1)
 
     return json.loads(output.stdout)
@@ -30,16 +30,16 @@ def known_google_sizes():
     """Return known Azure instance types."""
     with open("instance-types.json", "r") as json_file:
         json_types = json.loads(json_file.read())
-        return [type["size"] for type in json_types if type["cloud"] == "Google Cloud"]
+        return [type["size"] for type in json_types if type["cloud"] == "Google Cloud Platform"]
 
 
 known_types = known_google_sizes()
+
 missing_types = []
 for google_type in gce_instance_types():
-    if google_type["name"] not in known_types:
-        missing_types.append(
-            [google_type["name"], google_type["guestCpus"], google_type["memoryMb"]]
-        )
+    new = [google_type["name"], google_type["guestCpus"], google_type["memoryMb"]]
+    if google_type["name"] not in known_types and new not in missing_types:
+        missing_types.append(new)
 
 if missing_types:
     print(
